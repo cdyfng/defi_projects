2c43657c2b5c2a04187cb1db14dbb10ac1b25704
c fong <cfong@cdeMacBook-Pro.local>
2021-03-02 10:25:55 +0800

    multi-coins warningl

--- dex/mdex2/heco_mdx_bot/scripts/price.js
+++ dex/mdex2/heco_mdx_bot/scripts/price.js
@@ -6,33 +6,56 @@ const cmd = require("node-cmd");
 const oracleAddress = "0x7b4B0d9Cd226017eA3875D49196Ea63A6ea8C278";
 const usdtAddress = "0xa71edc38d189767582c38a3145b5873052c3e47a";
 const mdxAddress = "0x25D2e80cB6B86881Fd7e07dd263Fb79f4AbE033c";
+const dotAddress = "0xa2c49cee16a5e5bdefde931107dc1fae9f7773e3";
+
 //abi
 const oracleAbi = require("./oracle.json");
 // contract
 const provider = new web3(hecoAddress);
 const orcalContract = new provider.eth.Contract(oracleAbi, oracleAddress);
 
-const getMDXPrice = async () => {
-  const mdxPrice = await orcalContract.methods
-    .consult(mdxAddress, String(Math.pow(10, 18)), usdtAddress)
+let m = new Map([
+  //name , address, decimals, lowPrice, highPrice
+  ["mdx", [mdxAddress, 18, 4.5, 5.5]],
+  ["dot", [dotAddress, 18, 35, 37]],
+  //[//3, 'three'],
+]);
+
+const getPrice = async (address, decimals) => {
+  const price = await orcalContract.methods
+    .consult(address, String(Math.pow(10, decimals)), usdtAddress)
     .call();
-  return mdxPrice / 1e18;
+  return price / Math.pow(10, decimals);
 };
 const sleep = (n) => new Promise((res, rej) => setTimeout(res, n));
 
-async function mainLoop() {
+async function check_price(k, v) {
+  try {
+    let price = await getPrice(v[0], v[1]);
+    if (price > v[3]) {
+      cmd.runSync("say " + k + "价格超过" + price.toFixed(2));
+      console.log(new Date() + "wwarning ......Price great than:", price);
+    }
+    if (price < v[4]) {
+      cmd.runSync("say " + k + "价格低于" + price.toFixed(2));
+      console.log(new Date() + "wwarning ......Price less than:", price);
+    }
+    console.log(new Date() + "Price :", price);
+  } catch (e) {
+    // statements to handle any exceptions
+    console.log(e); // pass exception object to error handler
+  }
+}
+//check_balance()
+//mainLoop();
+
+async function main() {
   while (1) {
     try {
-      let price = await getMDXPrice();
-      if (price > 9.5) {
-        cmd.runSync("say 价格超过" + price.toFixed(2));
-        console.log(new Date() + "wwarning ......Price great than:", price);
-      }
-      if (price < 7) {
-        cmd.runSync("say 价格低于" + price.toFixed(2));
-        console.log(new Date() + "wwarning ......Price less than:", price);
-      }
-      console.log(new Date() + "Price :", price);
+      m.forEach(async function (value, key) {
+        console.log(key + " = " + value);
+        check_price(key, value);
+      });
       await sleep(5000);
     } catch (e) {
       // statements to handle any exceptions
@@ -40,5 +63,4 @@ async function mainLoop() {
     }
   }
 }
-//check_balance()
-mainLoop();
+main();
