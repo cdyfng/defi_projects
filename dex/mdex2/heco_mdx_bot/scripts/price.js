86f458a6b9e82ddb23ec7b556f4738b38057d3ec
cdyfng <xiaofeng.chen2007@163.com>
2021-03-22 19:14:01 +0800

    price update

--- dex/mdex2/heco_mdx_bot/scripts/price.js
+++ dex/mdex2/heco_mdx_bot/scripts/price.js
@@ -7,6 +7,7 @@ const oracleAddress = "0x7b4B0d9Cd226017eA3875D49196Ea63A6ea8C278";
 const usdtAddress = "0xa71edc38d189767582c38a3145b5873052c3e47a";
 const mdxAddress = "0x25D2e80cB6B86881Fd7e07dd263Fb79f4AbE033c";
 const dotAddress = "0xa2c49cee16a5e5bdefde931107dc1fae9f7773e3";
+const lhbAddress = "0x8f67854497218043e1f72908ffe38d0ed7f24721";
 
 //abi
 const oracleAbi = require("./oracle.json");
@@ -16,8 +17,9 @@ const orcalContract = new provider.eth.Contract(oracleAbi, oracleAddress);
 
 let m = new Map([
   //name , address, decimals, lowPrice, highPrice
-  ["mdx", [mdxAddress, 18, 4.5, 5.5]],
-  ["dot", [dotAddress, 18, 35, 37]],
+  ["mdx", [mdxAddress, 18, 3.4, 4.3]], //7.9   6.3  5.9  5.7 5.2 4.6 4.89清算
+  ["dot", [dotAddress, 18, 32, 40]],
+  ["lhb", [lhbAddress, 18, 0.35, 0.5]],
   //[//3, 'three'],
 ]);
 
@@ -36,7 +38,7 @@ async function check_price(k, v) {
       cmd.runSync("say " + k + "价格超过" + price.toFixed(2));
       console.log(new Date() + "wwarning ......Price great than:", price);
     }
-    if (price < v[4]) {
+    if (price < v[2]) {
       cmd.runSync("say " + k + "价格低于" + price.toFixed(2));
       console.log(new Date() + "wwarning ......Price less than:", price);
     }
